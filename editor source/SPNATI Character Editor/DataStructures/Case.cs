513d99e1006ecd201d841667339a6404e9969cc1
Kobrad <doctorakimbo@gmail.com>
2024-04-04 22:22:46 -0400

    Revert private marker fiasco

--- editor source/SPNATI Character Editor/DataStructures/Case.cs
+++ editor source/SPNATI Character Editor/DataStructures/Case.cs
@@ -1615,43 +1615,6 @@ namespace SPNATI_Character_Editor
 					}
 				}
 
-				if (cond.Character == speaker.FolderName)
-				{
-					if (cond.NotSaidMarker != null)
-					{
-						foreach (Marker marker in speaker.Markers.Value.Values)
-						{
-							if (marker.Name == cond.NotSaidMarker)
-							{
-								if (marker.Scope == MarkerScope.Private)
-								{
-									cond.NotSaidMarker = null;
-								}
-								break;
-							}
-						}
-					}
-
-					if (cond.SaidMarker != null)
-					{ 
-						foreach (Marker marker in speaker.Markers.Value.Values)
-						{
-							MarkerOperator op;
-							string value;
-							bool perTarget;
-							string name = Marker.ExtractConditionPieces(cond.SaidMarker, out op, out value, out perTarget);
-							if (marker.Name == name)
-							{
-								if (marker.Scope == MarkerScope.Private)
-								{
-									cond.SaidMarker = null;
-								}
-								break;
-							}
-						}
-					}
-				}
-
 				if (cond.IsEmpty)
 				{
 					Conditions.RemoveAt(i);
